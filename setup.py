5fe8ae776ddadd75124a2f2beb825446c86ac566
Kyle Goetz <kylegoetz@gmail.com>
2018-02-08 22:14:03 -0600

    initial setup.py so can pip install

---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name='PyRow',
+    version='0.1',
+    description='Module for interfacing with Concept2 PM3/PM4 monitors',
+    packages=['pyrow'],
+    install_requires=['pyusb']
+)
